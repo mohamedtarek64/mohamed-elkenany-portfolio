31048991deb698c264180caa0db397e5c6a27ac2
Mohamed Elkenany <mohamed202202632@gmail.com>
2026-02-12 12:54:28 +0200

    chore: remove accidental data duplicate in api/health

--- src/app/api/health/data/index.ts
+++
@@ -1,6 +0,0 @@
-export { projects } from './projects';
-export { skills } from './skills';
-export { experience } from './experience';
-export { education } from './education';
-export { personalInfo } from './personal-info';
-export { socialLinks } from './social-links';
